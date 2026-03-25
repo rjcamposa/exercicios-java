dafd4d4679948edb1ccd778b30bd470aae664396
Rafael <rafaeljanuariorjc@gmail.com>
2026-03-25 20:34:53 -0300

    t rebase --continue Recuperando projeto apos erro do workspace

---
+++ src/Exercicio05Soma.java
@@ -0,0 +1,16 @@
+package exercicios;
+
+public class Exercicio05Soma {
+
+	public static void main(String[] args) {
+
+        int soma = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            soma = soma + i;
+        }
+
+        System.out.println(soma);
+
+    }
+}
